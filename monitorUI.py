c3db97692bc909520624394ef5101606ed5637d2
Steven Keyes <stevenrkeyes@gmail.com>
2013-02-02 01:50:59 -0500

    finished tweaking UI
    still need to update the calibration code to save data

--- monitorUI.py
+++ monitorUI.py
@@ -35,10 +35,10 @@ class monitorUI(Tkinter.Frame):
         self.tempLabel = Tkinter.Label(self)
         self.tempLabel["bg"] = "black"
         self.tempLabel["padx"] = 25
-        self.tempLabel["pady"] = 25
+        self.tempLabel["pady"] = 15
         self.tempLabel["fg"] = "purple"
         self.tempLabel["text"] = "TT" + u'\N{DEGREE SIGN}' + "F"
-        self.tempLabel["font"]=('Digital-7', 100)
+        self.tempLabel["font"]=("Helvetica", 100)
         self.tempLabel["anchor"]="center"
         
         # combobox widget with location
@@ -54,20 +54,18 @@ class monitorUI(Tkinter.Frame):
         # place to type temperature
         self.tempTypeBox = ttk.Entry(self)
         self.tempTypeBox["width"] = 5
-        #self.tempTypeBox["padx"] = 10
         
         # label afterward with degF
-        self.degFsign = ttk.Label(self)
-        #self.degFsign["background"] = "black"
+        self.degFsign = Tkinter.Label(self)
+        self.degFsign["bg"] = "black"
+        self.degFsign["fg"] = "white"
         self.degFsign["text"] = u'\N{DEGREE SIGN}' + "F"
-        #self.degFsign["padx"] = 10
         
         # button to submit new calibration
         self.calibrateButton = ttk.Button(self)        
         self.calibrateButton["text"] = "Recalibrate"
         self.calibrateButton["command"] = self.addCalibrationInput
         self.calibrateButton["width"] = 9
-        #self.calibrateButton["padx"] = 10
 
         # place all the widgets
         self.locationSelection.grid(row=1, column=0, ipadx=10)
@@ -113,7 +111,7 @@ win['bg'] = "black"
 win.title("Temperature Monitor")
 # syntax: root.geometry('AxB-C+D')
 # A is width, B height, C distance from right side of screen, D distance from top of screen
-win.geometry('440x300')
+win.geometry('375x250')
 
 UI = monitorUI(win)
 UI.startMonitor()
